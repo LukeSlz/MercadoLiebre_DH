6288b65694f2e805c646c72e7cade6403388c11b
Zokam-LS <lucassy07@outlook.com>
2022-05-25 10:18:04 -0500

    nodemon running properly

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname,'public')));
 
 app.listen(3000, () =>{
     console.log('Servidor funcionando en puerto 3000');
