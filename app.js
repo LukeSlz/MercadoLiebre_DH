14f1ccd5ff3af2ff8b5e0d4345a37db4dcb9c8ce
Zokam-LS <lucassy07@outlook.com>
2022-05-24 19:41:58 -0500

    Avance clase 14, Flexbox

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 
 app.listen(3000, () =>{
-    console.log('Servidor funcionando');
+    console.log('Servidor funcionando en puerto 3000');
 });
 
 app.get('/', (req,res)=>{
